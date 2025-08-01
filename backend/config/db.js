03cc4b54cc41c52e476edbb7c9fe5f0a111ae865
Harsh Kumar Jha <jhaharshkumar293@gmail.com>
2025-08-01 22:25:36 +0530

    Setup server and db url - 01

---
+++ backend/config/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import path from "path";
+import { fileURLToPath } from "url";
+import dotenv from "dotenv";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("Error while connecting with MongoDB:", error.message);
+    process.exit(1);
+  }
+};
---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.get("/health", (req, res) => {
+  try {
+    res.send("Server is up and running...");
+  } catch (err) {
+    throw new Error("Something went wrong...");
+  }
+});
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log(`Failed to Start Backend Server: ${err}`);
+  } else {
+    connectDB();
+    console.log(`Application started on the Port:${PORT}`);
+  }
+});
